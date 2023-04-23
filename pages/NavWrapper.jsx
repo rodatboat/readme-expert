085c083babfc4ec60380c62fc000d3807ea7cac8
Ronaldo Suarez Surface <ronaldo3055@gmail.com>
2023-04-23 16:38:50 -0400

    fix

--- pages/NavWrapper.jsx
+++ pages/NavWrapper.jsx
@@ -4,7 +4,6 @@ import NavBar from "./NavBar"
 export default function NavWrapper(props) {
     return (
         <div className="flex flex-col max-h-screen h-screen max-w-screen w-screen overflow-auto">
-            <div className="flex-none min-h-16 h-16 md:min-h-12 md:h-12 max-w-screen">
                 <NavBar {...props} />
             </div>
             {props.children}
