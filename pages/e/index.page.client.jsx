f4e780244a014b0003ac399ccc265883f804e838
Ronaldo Suarez Surface <ronaldo3055@gmail.com>
2023-04-23 17:27:59 -0400

    redo-ing editor

--- pages/e/index.page.client.jsx
+++ pages/e/index.page.client.jsx
@@ -6,9 +6,9 @@ import NavWrapper from "../NavWrapper";
 export { Page };
 
 export const documentProps = {
-  title: 'readme.expert | Editor',
-  desc: 'Editor'
-}
+  title: "readme.expert | Editor",
+  desc: "Editor",
+};
 
 function Page() {
   const [preview, setPreview] = useState(false);
@@ -102,48 +102,49 @@ details
   const changeMarkdown = (e) => {
     e.preventDefault();
     setMarkdown(e.target.value);
-  }
+  };
   const togglePreview = () => {
     setPreview(!preview);
-  }
+  };
 
   const downloadFile = () => {
     const element = document.createElement("a");
-    const file = new Blob([markdown], { type: 'text/plain' });
+    const file = new Blob([markdown], { type: "text/plain" });
     element.href = URL.createObjectURL(file);
     element.download = `README.md`;
     document.body.appendChild(element);
     element.click();
-  }
+  };
 
-  useEffect(() => { }, []);
+  useEffect(() => {}, []);
   return (
     <>
-      <NavWrapper togglePreview={togglePreview} preview={preview} downloadFile={downloadFile} >
-        <div className="flex flex-initial mx-auto px-3 sm:px-4 md:px-6 py-3 sm:py-4 md:py-6 overflow-hidden">
-          <div className="flex flex-col flex-initial md:flex-row md:gap-x-4 md:gap-y-0">
-            <div className={`relative flex-1 basis-1/2 max-w-full overflow-x-hidden rounded-xl overflow-y-scroll scrollbar-hide ${preview ? "hidden" : "block"} md:block border-2 border-primary bg-secondary`}>
-              <p className="z-50 sticky top-0 flex-1 rounded-t-xl text-sm py-1 font-bold text-center text-[#fff] bg-secondary border-2 border-l-0 border-r-0 border-t-0 border-b-1 border-primary uppercase">
-                Editor
-              </p>
-              <div
-                // data-color-mode="light"
-                className="h-min editor-bg"
-              >
-                <ExpertEditor markdown={markdown} onChange={changeMarkdown} language={"markdown"} />
-              </div></div>
-
-            <div className={`relative flex-1 basis-1/2 max-w-full overflow-x-hidden rounded-xl overflow-y-scroll scrollbar-hide ${preview ? "block" : "hidden"} md:block border-2 border-primary bg-secondary`}>
-              <p className="z-50 sticky top-0 flex-1 rounded-t-xl text-sm py-1 font-bold text-center text-[#fff] bg-secondary border-2 border-l-0 border-r-0 border-t-0 border-b-1 border-primary uppercase">
-                Preview
-              </p>
-
-              <div
-                // data-color-mode="light"
-                className="h-min editor-bg"
-              >
-                <ExpertPreview markdown={markdown} />
-              </div>
+      <NavWrapper
+        togglePreview={togglePreview}
+        preview={preview}
+        downloadFile={downloadFile}
+      >
+        <div className="flex flex-col gap-4 md:flex-row p-2 h-full">
+          <div className={`flex-1 border-2 border-primary ${preview ? "hidden" : "block"} md:block`}>
+            <p className="z-50 sticky top-0 text-sm py-1 font-bold text-center text-[#fff]">
+              Editor
+            </p>
+            <div
+              // data-color-mode="light"
+              className="editor-bg"
+            >
+              {/* <ExpertEditor markdown={markdown} onChange={changeMarkdown} language={"markdown"} /> */}
+            </div>
+          </div>
+          <div className={`flex-1 border-2 border-primary ${preview ? "block" : "hidden"} md:block`}>
+            <p className="z-50 sticky top-0 text-sm py-1 font-bold text-center text-[#fff]">
+              Preview
+            </p>
+            <div
+              // data-color-mode="light"
+              className="editor-bg"
+            >
+              {/* <ExpertPreview markdown={markdown} /> */}
             </div>
           </div>
         </div>
